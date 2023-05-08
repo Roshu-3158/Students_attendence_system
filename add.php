c8a23a8cf9c40101a363784b72e55438a8ec499a
Roshu-3158 <roshandnikam3158@gmail.com>
2023-05-08 10:13:45 +0530

    first commit

---
+++ add.php
@@ -0,0 +1,31 @@
+<?php
+    include("header.php");
+
+?>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h2> 
+            <a class="btn btn-success" href="add.php"> Add Student</a>
+            <a href="index.php" class="btn btn-info pull-right"> Back</a>
+        </h2>
+    </div>
+
+    <div class="panel-body">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="name">Student Name</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="name">Roll Number</label>
+                <input type="text" name="roll" id="roll" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <input type="submit" name="submit" value="submit" class="btn btn-primary" required>
+            </div>
+        </form>
+    </div>
+</div>
