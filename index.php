4d11d95e66e9f02730b8dcd3e9cf9b35258a35c8
Roshu-3158 <roshandnikam3158@gmail.com>
2023-05-11 18:14:17 +0530

    attendence insertion in database

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 include("db.php");
 include("header.php");
+$flag = 0;
+
+    if(isset($_POST['submit']))
+    {
+        foreach($_POST['attendence_status'] as $id=>$attendence_status)
+        {
+            $student_name=$_POST['student_name'][$id];
+            $roll_number=$_POST['roll_number'][$id];
+            $date=date("Y-m-d H:i:s");
+
+            $result = mysqli_query($conn,"insert into attendence_records(student_name,roll_number,attendence_status,date) values('$student_name','$roll_number','$attendence_status','$date')");
+
+            if($result){
+                $flag=1;
+            }
+
+        }
+
+    }
 ?>
 
 <div class="panel panel-default">
@@ -10,10 +29,18 @@ include("header.php");
             <a class="btn btn-success" href="add.php">Add student</a>
             <a class="btn btn-info pull-right" href="viewall.php">View All</a>
         </h2>
+
+        <?php if($flag){ ?>
+        <div class="alert alert-success">
+            Attendence Data is Successfully Inserted !
+        </div>
+        <?php }?>
     </div>
 
+    <h2> <div class="text-center"> Date:<?php echo date("Y-m-d")  ?></div> </h2>
+
     <div class="panel panel-body">
-        <form action="index.php">
+        <form action="index.php" method="post">
             <table class="table table-stripped">
                 <tr>
                     <th>Serial Number</th>
@@ -25,19 +52,26 @@ include("header.php");
                 <?php
                 $result = mysqli_query($conn, "select * from attendence");
                 $serialnumber = 0;
+                $counter = 0;
                 while ($row = mysqli_fetch_array($result)) {
                     $serialnumber++;
                 ?>
                     <tr>
                         <td> <?php echo $serialnumber; ?></td>
+
                         <td> <?php echo $row['student_name']; ?></td>
+                        <input type="hidden" value="<?php echo $row['student_name']; ?>" name="student_name[]">
+                        
                         <td> <?php echo $row['roll_number']; ?></td>
+                        <input type="hidden" value="<?php echo $row['roll_number']; ?>" name="roll_number[]">
+
                         <td>
-                            <input type="radio" name="" value="Present"> Present </input> 
-                            <input type="radio" name="" value="Absent"> Absent </input>
+                            <input type="radio" name="attendence_status[<?php echo $counter; ?>]" value="Present"> Present </input> 
+                            <input type="radio" name="attendence_status[<?php echo $counter; ?>]" value="Absent"> Absent </input>
                         </td>
                     </tr>
                 <?php
+                $counter++;
                 }
 
                 ?>
