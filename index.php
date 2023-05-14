ae98cd471d99cb37930a40c214a0e23c192eef39
Roshu-3158 <roshandnikam3158@gmail.com>
2023-05-14 16:07:24 +0530

    update student attendence in the database

--- index.php
+++ index.php
@@ -2,22 +2,43 @@
 include("db.php");
 include("header.php");
 $flag = 0;
-
+$update=0;
     if(isset($_POST['submit']))
     {
-        foreach($_POST['attendence_status'] as $id=>$attendence_status)
-        {
-            $student_name=$_POST['student_name'][$id];
-            $roll_number=$_POST['roll_number'][$id];
-            $date=date("Y-m-d H:i:s");
+        $date=date("Y-m-d");
 
-            $result = mysqli_query($conn,"insert into attendence_records(student_name,roll_number,attendence_status,date) values('$student_name','$roll_number','$attendence_status','$date')");
+        $records = mysqli_query($conn,"select * from attendence_records where date='$date'");
+        $num = mysqli_num_rows($records);
 
-            if($result){
-                $flag=1;
+        if($num){
+            foreach($_POST['attendence_status'] as $id=>$attendence_status)
+            {
+                $student_name=$_POST['student_name'][$id];
+                $roll_number=$_POST['roll_number'][$id];
+
+                $result = mysqli_query($conn,"update attendence_records set student_name=$'student_name',roll_number='$roll_number',attendence_status='$attendence_status',date='$date' where date='$date'" );
+                if($result){
+                    $update=1;
+                }
+
             }
 
+
         }
+        else{
+            foreach($_POST['attendence_status'] as $id=>$attendence_status)
+            {
+                $student_name=$_POST['student_name'][$id];
+                $roll_number=$_POST['roll_number'][$id];
+
+                $result = mysqli_query($conn,"insert into attendence_records(student_name,roll_number,attendence_status,date) values('$student_name','$roll_number','$attendence_status','$date')");
+
+                if($result){
+                    $flag=1;
+                }
+
+            }
+    }
 
     }
 ?>
@@ -34,7 +55,14 @@ $flag = 0;
         <div class="alert alert-success">
             Attendence Data is Successfully Inserted !
         </div>
+
         <?php }?>
+        <?php if($update){ ?>
+        <div class="alert alert-success">
+            Student Attendence Upadated Successfully !
+        </div>
+        <?php }?>
+
     </div>
 
     <h2> <div class="text-center"> Date:<?php echo date("Y-m-d")  ?></div> </h2>
